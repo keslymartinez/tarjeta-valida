4b6dea381c490ba237e8b9c46b72e6a6fa045308
kariambar <karina.spalacios@gmail.com>
2017-10-05 11:50:37 -0300

    mi primer proyecto

---
+++ app.js
@@ -0,0 +1,32 @@
+
+do{ 
+ 		var num = prompt("ingrese su numero de tarjeta de credito");  //Mi prompt pide numero de tarjeta al usuario y no deja entrar espacios vacios		 
+ }while(num < 0);
+  	  
+var num =0;
+Function invertir() // esta funcion invierte los numeros de atras para adelante
+	for (var i = 0 ; i <= array.length ; i+2);{
+		array.push
+	}
+	var numpar =[];
+	var numimpar =[];
+	if([i]%2==0){  // este modulo divide mi numero par
+    numpar.push[i];
+
+   frase=pares[i].split(num1,num2);// tranforma mi frase en numero
+                        
+     var num1= parseInt[1]; //declaro mi variable para sumar 
+      var num2= parseInt[2];
+       sum=[uno+dos];
+                        
+        sum= (sum2[i]);
+                        
+                }
+            }        
+       if(sum%10===0){  // se saca el residuo a travez de un modulo
+       return "Su tarjeta es válida";
+       }else{
+      return "Su tarjeta es inválida";
+                    }
+
+}
